342b2b9a512e1b804f11f64313cede62fe85cf81
janamkusuma <janamkusuma@gmail.com>
2026-01-19 23:55:59 +0530

    Fix port + add PyPDF2

--- app.py
+++ app.py
@@ -258,5 +258,8 @@ def reports():
     return render_template("reports.html", reports=data)
 
 # ---------------- RUN ----------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
